8c056d42e7f5c01eb41fa9e7c6898716899625f4
Jake Stambaugh <jake.stambaugh@gmail.com>
2019-01-25 23:06:44 -0500

    Fixed WallArranger if a plane object is in scene at the required depth

--- HomePlanet/Assets/WallArranger.cs
+++ HomePlanet/Assets/WallArranger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WallArranger : MonoBehaviour {
 
-  float wallZ = 0f;
+  public Transform backPlane;
 
 	public Transform left;
 	public Transform right;
@@ -12,11 +12,14 @@ public class WallArranger : MonoBehaviour {
 	public Transform bottom;
 
 	// Use this for initialization
-	void Start () {
-		Camera camera = GetComponent<Camera>();
-		left.position = camera.ViewportToWorldPoint(new Vector3(0, 0.5f, wallZ));
-		right.position = camera.ViewportToWorldPoint(new Vector3(1, 0.5f, wallZ));
-		bottom.position = camera.ViewportToWorldPoint(new Vector3(0.5f, 0, wallZ));
-		top.position = camera.ViewportToWorldPoint(new Vector3(0.5f, 1, wallZ));
+  private new Camera camera;
+  void Start () {
+    camera = GetComponent<Camera>();
+  }
+	void Update () {
+		left.position = camera.ViewportToWorldPoint(new Vector3(0, 0.5f, backPlane.position.z - transform.position.z));
+		right.position = camera.ViewportToWorldPoint(new Vector3(1, 0.5f, backPlane.position.z - transform.position.z));
+		bottom.position = camera.ViewportToWorldPoint(new Vector3(0.5f, 0, backPlane.position.z - transform.position.z));
+		top.position = camera.ViewportToWorldPoint(new Vector3(0.5f, 1, backPlane.position.z - transform.position.z));
 	}
 }
