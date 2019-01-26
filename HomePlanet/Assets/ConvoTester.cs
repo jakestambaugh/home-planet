f67a332c72aa2bd0360e7f62cde4fb0c6adbed94
Jake Stambaugh <jake.stambaugh@gmail.com>
2019-01-26 10:50:05 -0500

    Conversation tester

---
+++ HomePlanet/Assets/ConvoTester.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ConvoTester : MonoBehaviour
+{
+    public TextMeshProUGUI questionText;
+
+    public void SayYes() {
+        questionText.text = "This is your new home planet!";
+    }
+
+    public void SayNo() {
+        questionText.text = "Get out of here!";
+    }
+}
