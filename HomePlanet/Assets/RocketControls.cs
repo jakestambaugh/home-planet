2d5e634272f79b1473f2d21f0071a2f5280c7613
erinlhiggins <elh76@pitt.edu>
2019-01-25 21:59:36 -0500

    two planets now

---
+++ HomePlanet/Assets/RocketControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketControls : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float thrust = 5;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1")) {
+            rb.AddForce(this.transform.up * thrust, ForceMode2D.Force);
+        }
+	}
+}
