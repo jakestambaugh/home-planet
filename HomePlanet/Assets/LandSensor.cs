07def37e63b3e51c7e8ecfb1e13388800364d351
erinlhiggins <elh76@pitt.edu>
2019-01-26 12:04:53 -0500

    pick up

---
+++ HomePlanet/Assets/LandSensor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandSensor : MonoBehaviour
+{
+    private Collider2D cd;
+    public GameObject bubble;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cd = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider2D[] nearby = Physics2D.OverlapCircleAll(transform.position, 8);
+        foreach(Collider2D check in nearby){
+            if(cd.IsTouching(check)){
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up * -1);
+                if(hit.collider == check){
+                    bubble.SetActive(false);
+                }
+            }
+        }
+    }
+}
