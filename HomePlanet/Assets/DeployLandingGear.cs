4cd026b1458337dfe64f0c4ab97b6e4c38ac52b3
Brandon Marvenko <bmarvenko@zoll.com>
2019-01-26 18:00:26 -0500

    Landing Gear now only deploys if the rocket is in the appropriate orientation for landing.

--- HomePlanet/Assets/DeployLandingGear.cs
+++ HomePlanet/Assets/DeployLandingGear.cs
@@ -9,9 +9,7 @@ public class DeployLandingGear : MonoBehaviour
     bool deployed = false;
     float animTime = 0.0f;
     float animRate = 0.02f;
-
     bool nearPlanet = false;
-    int nearcount = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -39,10 +37,14 @@ public class DeployLandingGear : MonoBehaviour
             float distToPlanet = Vector3.Distance(transform.position, planet.transform.position);
             float planetRadius = planet.GetComponent<CircleCollider2D>().bounds.size.x/2;
             float distToPlanetSurface = distToPlanet-planetRadius;
+
+            Vector3 heading =   transform.position - planet.transform.position;
+            Vector3 rocketOri = transform.TransformDirection(Vector3.up);
+            var dotProduct = Vector3.Dot(heading, rocketOri);
             
             //Debug.Log("ypos: " + transform.position.y + ", distToPlanet: " + distToPlanet + ", planetRadius: " + planetRadius + ", distToPlanetSurface: " + distToPlanetSurface);
 
-            if(distToPlanetSurface < 1.5)
+            if(distToPlanetSurface < 1.5 && (dotProduct > (distToPlanet-0.4f) && dotProduct < (distToPlanet+0.4f)))
             {
                 nearPlanet = true;
             }
