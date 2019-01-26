043f79ed368e1088f82eacb48ec7e0ed52fbcb12
Jake Stambaugh <jake.stambaugh@gmail.com>
2019-01-26 15:26:21 -0500

    Add planet and planet data

---
+++ HomePlanet/Assets/Scripts/Planet.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    PlanetData data;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.tag = "Planet";
+    }
+
+    private string DescribePeople() {
+        return "cat-like creatures";
+    }
+
+    private string DescribeColor () {
+        return "blue";        
+    }
+
+    private string DescribeSize() {
+        return "very big";
+    }
+
+    private string Intro() {
+        string[] possibleIntros = { 
+            "I'm from a planet that ",
+            "My planet ",
+            "Please take me to a planet that ",
+            "I would like to go to a planet that ",
+            "My home is a planet that ",
+            "My home planet ",
+            "The planet that I'm from "
+        };
+        int i = Random.Range(0, possibleIntros.Length);
+        return possibleIntros[i];
+    }
+
+    public string GetClue() {
+        string[] possibleClues = {
+            "has a " + DescribeColor() + " color!",
+            "is very " + DescribeColor() + ".",
+            "is a " + DescribeSize() + " planet.",
+            "is full of " + DescribePeople() + "."            
+        };
+        int i = Random.Range(0, possibleClues.Length);
+
+        return "" + Intro() + possibleClues[i];
+    }
+}
---
+++ HomePlanet/Assets/Scripts/PlanetData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Planet", menuName = "Create Planet", order = 1)]
+public class PlanetData : ScriptableObject 
+{
+    public Transform location;
+
+    public float radius;
+
+    public float gravityRadius;
+    public float gravityScale = 1.0f;
+
+    public Texture texture;
+}
