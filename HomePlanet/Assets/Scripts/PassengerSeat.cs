6017d02d4cabed15bda73d6d3509efd5ecd594d6
Jake Stambaugh <jake.stambaugh@gmail.com>
2019-01-26 14:26:34 -0500

    Add PassengerSeat script

---
+++ HomePlanet/Assets/Scripts/PassengerSeat.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassengerSeat : MonoBehaviour
+{
+    PassengerLifecycle passenger;
+
+    public void LoadPassenger(PassengerLifecycle p) {
+        if (passenger == null) {
+            passenger = p;
+        }
+    }
+
+    public string GetPassengerClue() {
+        return passenger.GetHomeworldClue();
+    }
+
+    public GameObject GetPassengerHomeworld() {
+        return passenger.GetHomeworld();
+    } 
+
+    public PassengerLifecycle UnloadPassenger() {
+        PassengerLifecycle p = passenger;
+        passenger = null;
+        return p;
+    }
+}
