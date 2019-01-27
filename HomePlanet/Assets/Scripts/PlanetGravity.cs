46e842c6eabf8dc360dfa81a0e81a13895d8bc42
Brandon Marvenko <bmarvenko@zoll.com>
2019-01-26 20:13:31 -0500

    Added planet gravity script and updated rocket collider

---
+++ HomePlanet/Assets/Scripts/PlanetGravity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGravity : MonoBehaviour
+{
+    Component[] pe2d;
+    public float magnitude = -6.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float planetRadius = GetComponent<CircleCollider2D>().bounds.size.x/2;
+        pe2d = GetComponentsInChildren<PointEffector2D>();
+        
+        foreach (PointEffector2D pef in pe2d)
+        {
+            pef.forceMagnitude = planetRadius*magnitude;
+        }
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
--- HomePlanet/Assets/Scripts/RocketControls.cs
+++ HomePlanet/Assets/Scripts/RocketControls.cs
@@ -15,7 +15,7 @@ public class RocketControls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButton("Fire1")) {
+        if (Input.GetButton("Fire2")) {
             rb.AddForce(this.transform.up * thrust, ForceMode2D.Force);
         }
         if(Input.GetAxis("Horizontal") > .4){
