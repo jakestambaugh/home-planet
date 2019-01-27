a6e8a38aa8173cf2e2d14bebd37c62e0f0b8010f
Brandon Marvenko <bmarvenko@zoll.com>
2019-01-27 09:22:38 -0500

    Added camera follow script

---
+++ HomePlanet/Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+        Vector3 camPos = new Vector3 (playerPos.x, playerPos.y, -7.0f);
+
+        transform.position = camPos;
+    }
+}
