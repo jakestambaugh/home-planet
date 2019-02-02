f70842ca5f5557756eb666eec1048c9f13bd22fa
Jake Stambaugh <jake.stambaugh@gmail.com>
2019-02-02 17:33:11 -0500

    Refactor timer to count down to zero

--- HomePlanet/Assets/Scripts/Timer.cs
+++ HomePlanet/Assets/Scripts/Timer.cs
@@ -9,44 +9,39 @@ public class Timer : MonoBehaviour
     public TextMeshProUGUI clueText;
     public TextMeshProUGUI timerText;
     public TextMeshProUGUI scoreText;
-    private float startTime;
+    public float timerLength;
     private int score;
-    private bool hit = false;
     private HighScore highScore;
     // Start is called before the first frame update
     void Start()
     {
-        startTime = Time.time;
         score = 0;
-        hit = false;
         highScore = GetComponent<HighScore>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float t = Time.time - startTime;
-        string seconds = " ";
-        string minute = (2 - ((int)t / 60)).ToString();
-        if ((58 - (t % 60) + 1) > 10)
-        {
-            seconds = (58 - (t % 60) + 1).ToString("f0");
-        }
-        else
-        {
-            seconds = "0" + (58 - (t % 60) + 1).ToString("f0");
-        }
-        if ((2 - (int)t / 60) <= 0 && (58 - (t % 60  + 1) <= 0) && hit == false)  {
-            hit = true;
+        timerLength -= Time.deltaTime;
+        if(timerLength <= 0f) {
             highScore.SetHighScore(score);
             clueText.SetText("Press P to restart!");
             Time.timeScale = 0f;
         }
-        if ((58 - (t % 60) + 1) > 0 && !seconds.Equals("010"))
-        {
-            timerText.text = "Time: " + minute + ":" + seconds;
-            scoreText.text = "Score: " + score;
+        else {
+            UpdateText(RenderTime(timerLength));
         }
+    }
+
+    private string RenderTime(float delta) {
+        float minutes = Mathf.Floor(delta / 60);
+        float seconds = Mathf.RoundToInt(delta % 60);
+        return string.Format("{0:0}:{1:00}", minutes, seconds);
+    }
+
+    private void UpdateText(string minutesAndSeconds) {
+        timerText.text = "Time: " + minutesAndSeconds;
+        scoreText.text = "Score: " + score;
     }
 
     public void UpdateScore(){
